2fcb12353e9cac33c1223dc8c21cdf34e252409b
Charlee Gordon <cgordon5025@gmail.com>
2024-05-29 16:49:17 -0400

    Updates

---
+++ environment.js
@@ -0,0 +1,11 @@
+window.__env = {
+    "APP_ID": "0.0.0.0.0",
+    "APP_NAME": "text-to-speech-test",
+    "APP_VERSION": "0.1.0",
+    "APP_PACKAGE_HASH": "fb09657f6b8f396fe907d4da94fd0a20ead76222a5eb9efc3b3af749a3023bdd",
+    "APP_PACKAGE_HASH_ID": "f0676836-e0dd-9f02-ed62-ae9f33f4a03d",
+    "APP_HASH": "bd2e984592d749596f0b9ed142dd78639f670c86",
+    "SKIP_PREFLIGHT_CHECK": "true",
+    "API_URL": "https://localhost:3001",
+    "PUBLIC_URL": "/public"
+};
